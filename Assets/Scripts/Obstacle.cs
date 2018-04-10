430448249f7d70e0f7babb2fa0b9117aebf55d93
Escher <iconescher@gmail.com>
2018-04-10 10:43:07 +0200

    Master commit
    prima commit lul

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+	public PlayerManager pm;
+
+	void OnColliderEnter2D(Collider2D col)
+	{
+		pm.direction = Vector2.zero;
+	}
+
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+	[Header("Player Settings")]
+	public float speed = 5;
+	[SerializeField]
+	bool movingHor;
+	[SerializeField]
+	bool movingVer;
+	public Vector2 direction;
+
+
+	// Use this for initialization
+	void Start () {
+		direction = Vector2.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetInput ();
+		Move ();
+	}
+
+	private void GetInput()
+	{
+		direction = Vector2.zero;
+		StartCoroutine (JustWait ());
+
+			if (Input.GetKey (KeyCode.W) && !movingHor) {
+				direction += Vector2.up;
+				movingVer = true;
+			}
+			if (Input.GetKey (KeyCode.S) && !movingHor) {
+				direction += Vector2.down;
+				movingVer = true;
+			}
+			if (Input.GetKey (KeyCode.A) && !movingVer) {
+				direction += Vector2.left;
+				movingHor = true;
+			}
+			if (Input.GetKey (KeyCode.D) && !movingVer) {
+				direction += Vector2.right;
+				movingHor = true;
+			}
+
+		}
+
+
+	IEnumerator JustWait()
+	{
+		yield return new WaitForSeconds (0.2f);
+		if (direction == Vector2.zero) {
+			movingVer = false;
+			movingHor = false;
+		}
+
+			
+	}
+
+	private void Move()
+	{
+		this.gameObject.transform.Translate (direction * speed * Time.deltaTime);
+	}
+
+
+}
