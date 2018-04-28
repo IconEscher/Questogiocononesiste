74713bc682d5d57d939b80bf0be4b1d27128ba38
Escher <iconescher@gmail.com>
2018-04-28 16:06:56 +0200

    UI
    UI

--- Assets/Scripts/ObjectiveManager.cs
+++ Assets/Scripts/ObjectiveManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectiveManager : MonoBehaviour {
 
@@ -8,16 +9,18 @@ public class ObjectiveManager : MonoBehaviour {
 	public GameObject[] piante;
 	public int arrayIndex = 0;
 	public bool isGrowing = false;
+	public GameObject textWin;
 		
 	void OnTriggerStay2D(Collider2D col)
 	{
 		if (col.gameObject.tag == "Player") {
-			Debug.Log ("Sto collidendo");
-			if (!isGrowing && arrayIndex < 6)
+			//Debug.Log ("Sto collidendo");
+			if (!isGrowing && arrayIndex < 5)
 				StartCoroutine (StartPesto ());
-		} else if (arrayIndex == 5) 
+		} 
+		if (arrayIndex == 5) 
 		{
-			
+			textWin.SetActive (true);
 		}
 
 	}
---
+++ Assets/health.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class health : MonoBehaviour {
+
+	public Image currentHealthBar;
+	float hitPoint = 150;
+	float maxHitPoint = 150;
+
+
+void Start()
+{
+	UpdateHealthBar();
+}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			TakeDamage (15);
+		}
+	}
+
+void UpdateHealthBar()
+{
+		float ratio = hitPoint / maxHitPoint;
+		currentHealthBar.rectTransform.localScale = new Vector3 (1, ratio, 1);
+}
+		
+	void TakeDamage(float damage)
+	{
+		hitPoint -= damage;
+		UpdateHealthBar ();
+	}
+
+}
